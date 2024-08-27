e24b2167823570e718b9b7b091e443384dc50fa6
soham biswas <sohambiswas05@gmail.com>
2024-08-27 12:59:45 +0530

    Update Vite config to bind to all network interfaces

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // Binds to 0.0.0.0
+    port: 4173,   // Choose a port number
+  },
 })
